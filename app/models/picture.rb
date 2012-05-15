c8a2230dbda8fdf57c01801b4c0f4c8a665f9736
monaFHM <studium@ramonaleenings.com>
2012-05-15 13:30:54 +0200

    Pictures Path changed

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,8 +1,10 @@
 class Picture < ActiveRecord::Base
   belongs_to :artist
-  has_attached_file :avatar #,:styles => { :medium => "300x300>", :thumb => "100x100>" }, :url  => "/assets/products/:id/:style/:basename.:extension"
-    #,:processors => [:thumbnail, :time_stamper],
-     #:date_format => "%Y%m%d%H%M%S"
+  has_attached_file :avatar ,:url => "/pictures/:class/:attachment/:id/:style/:basename.:extension"
+      #,:path => ":rails_root/public/pictures/:class/:attachment/:id/:style/:basename.:extension"
+#,:styles => { :medium => "300x300>", :thumb => "100x100>" }, :url  => "/assets/products/:id/:style/:basename.:extension"
+#,:processors => [:thumbnail, :time_stamper],
+#:date_format => "%Y%m%d%H%M%S"
 
  validates :name, :presence => true
  #validates :url, uniqueness => true
